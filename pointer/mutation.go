8a6c3c5a8f9e5ff028f97e9c43b237853825b12c
Ricky Raz <ricky@saas-studios.com>
2025-11-09 22:53:52 +0700

    add examp

--- pointer/mutation.go
+++ pointer/mutation.go
@@ -1,6 +1,9 @@
 package pointer
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func change(x *int) {
 	*x = 100
@@ -19,4 +22,21 @@ func TestMutation() {
 	// Benar: pass alamat variabel menggunakan &
 	change(&n)
 	fmt.Println("Sesudah:", n) // Output: 100
+
+	// ----
+
+	// Immutable - value copy
+	x := 5
+	y := x
+	y = 10 // x tetap 5
+
+	// Mutable via pointer
+	person := struct{ name string }{"Ricky"}
+	log.Println("person1", person)
+	p := &person
+	p.name = "Adi" // person juga berubah
+
+	log.Println("y", y)
+	log.Println("person2", person)
+	log.Println("p", p)
 }
